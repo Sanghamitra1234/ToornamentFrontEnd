b8e66268aaccb5c35cd55b1e6619b0266d1ca6f2
Sanghamitra1234 <sanghota4567@gmail.com>
2019-12-23 17:57:43 +0530

    first commit

---
+++ build/precache-manifest.7aa6eb4645ea49e108eeda5d668832ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc650873c4922f67a8fccb29512b73b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdc5ef083be522c347b8",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bc19b89861502fb49de4",
+    "url": "/static/css/main.15943793.chunk.css"
+  },
+  {
+    "revision": "bdc5ef083be522c347b8",
+    "url": "/static/js/2.6e0d22bf.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.6e0d22bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc19b89861502fb49de4",
+    "url": "/static/js/main.ff61d438.chunk.js"
+  },
+  {
+    "revision": "e467434bf39517a31554",
+    "url": "/static/js/runtime-main.df69f0b1.js"
+  }
+]);
